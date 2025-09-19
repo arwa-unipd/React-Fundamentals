30953ad34cdb32efb981cb32c54b59fff0bed660
arwaUnox <arwa.fahoud@unox.com>
2025-09-19 12:17:44 +0200

    add footer services and tours

--- backroads-app/src/App.tsx
+++ backroads-app/src/App.tsx
@@ -1,6 +1,9 @@
 import About from './components/About'
+import Footer from './components/Footer'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import Services from './components/Services'
+import Tours from './components/Tours'
 
 import './styles.css'
 
@@ -10,6 +13,9 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Services />
+      <Tours />
+      <Footer />
     </>
   )
 }
